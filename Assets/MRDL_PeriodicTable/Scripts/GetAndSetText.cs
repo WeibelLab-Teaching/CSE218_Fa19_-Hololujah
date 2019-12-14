7d54bef0d64dcb344e5130c4688d9ac7fe5df243
dheeru487 <dharmendra_chaturvedi@outlook.com>
2019-12-13 23:53:45 -0800

    dev branch committed
    final

---
+++ Assets/MRDL_PeriodicTable/Scripts/GetAndSetText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace HoloToolkit.MRDL.PeriodicTable
+{
+	
+	public class GetAndSetText : MonoBehaviour
+	{
+		public InputField Dishname;
+		public InputField Categoryname;
+		public Text fText;
+	
+		public void setget()
+		{
+			fText.text = "Dish name you entered is: " + Dishname.text + " and Category name is: " + Categoryname.text;
+		}
+	}
+}
